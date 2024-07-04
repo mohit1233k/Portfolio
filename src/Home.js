438397b180cd22c07b47ba378b6d3c67ec6b9fbf
ritesh1711 <ritesh098765@gmail.com>
2024-07-04 23:50:35 +0530

    updated logo

--- src/Home.js
+++ src/Home.js
@@ -2,8 +2,8 @@ import React from 'react'
 
 export default function home() {
   return (
-    <div>
-      this is a website Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sed commodi accusamus rem debitis itaque consectetur fuga velit illum odio corrupti neque ut earum molestias ipsa consequuntur recusandae, sint laudantium sit!
+    <div className='bg-black h-100vh'>
+      
     </div>
   )
 }
--- src/Navbar.js
+++ src/Navbar.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import logo from "./instagram_profile_image.png"
 
 export default function Home() {
   return (
-    <div className='bg-red-200 w-full h-36 flex flex-row'>
-      <div className="w-1/4 flex justify-between items-center b" >
-        <img src='E:\coding\portfolio\src\instagram_profile_image.png' alt='LOGO'></img>
+    <div className='bg-red-200 w-full h-36 flex flex-row mx-14 '>
+      <div className="w-1/4 flex justify-between items-center b "  >
+      <img src={logo} alt="Logo" className="h-36 w-40 m-5 p-5" />
       </div>
       <div className="w-3/4 flex justify-around items-center p-14 pl-5 font-bold ">
       <div>
